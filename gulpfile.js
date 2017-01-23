3ef9e8dc4980d07c392d75900d22519e41ab3cde
Ivan Kovic <kovic.ivan@gmail.com>
2017-01-23 08:34:06 +0100

    React setup done

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserify = require('browserify');  // Bundles JS.
+var babelify = require('babelify');  // Transforms React JSX to JS.
+var source = require('vinyl-source-stream');
+
+// source & build paths
+var paths = {
+  CSS_SRC: './src/**/*.scss',
+  CSS_MAIN: './src/main.scss',
+  CSS_BUILD: './static/css/',
+  JS_MAIN: './src/app.js',
+  JS_SRC: './src/**/*.js',
+  JS_BUILD: './static/js'
+};
+
+// sass compile
+gulp.task('styles', function () {
+  gulp.src(paths.CSS_MAIN)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.CSS_BUILD))
+});
+
+// Our JS task. It will Browserify our code and compile React JSX files.
+gulp.task('js', function () {
+  browserify(paths.JS_MAIN)
+    .transform(babelify, {presets: ["es2015", "react"]})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(paths.JS_BUILD));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function () {
+  gulp.watch(paths.CSS_MAIN, ['styles']);
+  gulp.watch(paths.CSS_SRC, ['styles']);
+  gulp.watch(paths.JS_SRC, ['js']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'styles', 'js']);
+
+// The production build task
+gulp.task('prod', ['styles', 'js']);
--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'views/index.html'),
+    pathname: path.join(__dirname, 'static/index.html'),
     protocol: 'file:',
     slashes: true
   }));
@@ -27,7 +27,7 @@ function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    win = null;
   });
 }
 
@@ -41,7 +41,7 @@ app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 });
 
@@ -49,7 +49,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 });
 
